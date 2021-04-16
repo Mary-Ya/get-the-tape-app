15d6dd7b9b297959d9a731888df00776a8a3e71b
Mary Y <notamary@bk.ru>
2021-04-16 15:25:10 +0200

    fix randomizer

--- server.js
+++ server.js
@@ -39,32 +39,46 @@
  app.use(express.static(__dirname + '/public'))
     .use(cors())
     .use(cookieParser());
+  var reFetchCounter = 0;
+  var newRequest = false;
+  function trackRequest (req, res) {
+    var access_token = req.query.access_token;
+    var {market, offset, q} = req.query;
+  
+    var reqOptions = {
+       url: `https://api.spotify.com/v1/search?q=${generateRandomString(1)}&type=track&market=${market}&limit=1&offset=${offset}`,
+       headers: { 'Authorization': `Bearer ${access_token}` },
+       json: true
+     };
+
+    return request.get(reqOptions, function(error, response, body) {
+      console.log('error', error)
+        if (!error && response.statusCode === 200) {
+          // console.log('response.body.tracks.items[0]', response.body.tracks.items[0]);
+          if (!response.body.tracks.items[0].preview_url && reFetchCounter < 4) {
+            console.log('no preview: ' + reFetchCounter);
+            reFetchCounter++;
+            setTimeout(trackRequest, 1000, req, res);
+          } else {
+            reFetchCounter = 0;
+            res.send(JSON.stringify(
+              response
+            ));
+          }
+        } else {
+          reFetchCounter = 0;
+          res.send('/error' +
+            JSON.stringify(
+              response
+            ));
+        }
+     });
+  }
  
  app.get('/get_random_track', function(req, res) {
-  var access_token = req.query.access_token;
-  var {market, offset, q} = req.query;
-  
-   var reqOptions = {
-     url: `https://api.spotify.com/v1/search?q=${q}&type=track&market=${market}&limit=1&offset=${offset}`,
-     headers: { 'Authorization': `Bearer ${access_token}` },
-     json: true
-   };
-   console.log('reqOptions', reqOptions);
-   console.log('req.query', req.query);
-   request.get(reqOptions, function(error, response, body) {
-    console.log('error', error)
-      if (!error && response.statusCode === 200) {
-        console.log('body', body);
-        res.send(JSON.stringify(
-            response
-          ));
-      } else {
-        res.send('/error' +
-          JSON.stringify(
-            response
-          ));
-      }
-   });
+
+   
+   trackRequest(req, res);
  });
 
  app.get('/login', function(req, res) {
