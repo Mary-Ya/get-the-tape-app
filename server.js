0810b089bdbcfe38991b3928065b9be45cbf6f4d
Mary Y <notamary@bk.ru>
2021-04-16 10:36:13 +0200

    add fetching random track

--- server.js
+++ server.js
@@ -40,13 +40,40 @@
     .use(cors())
     .use(cookieParser());
  
+ app.get('/get_random_track', function(req, res) {
+  var access_token = req.query.access_token;
+  var {market, offset, q} = req.query;
+  
+   var reqOptions = {
+     url: `https://api.spotify.com/v1/search?q=${q}&type=track&market=${market}&limit=1&offset=${offset}`,
+     headers: { 'Authorization': `Bearer ${access_token}` },
+     json: true
+   };
+   console.log('reqOptions', reqOptions);
+   console.log('req.query', req.query);
+   request.get(reqOptions, function(error, response, body) {
+    console.log('error', error)
+      if (!error && response.statusCode === 200) {
+        console.log('body', body);
+        res.send(JSON.stringify(
+            response
+          ));
+      } else {
+        res.send('/error' +
+          JSON.stringify(
+            response
+          ));
+      }
+   });
+ });
+
  app.get('/login', function(req, res) {
  
    var state = generateRandomString(16);
    res.cookie(stateKey, state);
  
    // your application requests authorization
-   var scope = 'user-read-private user-read-email';
+   var scope = 'user-read-private user-read-email streaming';
    res.redirect('https://accounts.spotify.com/authorize?' +
      querystring.stringify({
        response_type: 'code',
