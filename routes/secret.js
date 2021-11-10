fe8db5d70397ee53811c102e92bb9c0bbe15a6e2
Mary Y <notamary@bk.ru>
2021-11-10 23:29:11 +0100

    merge

---
+++ routes/secret.js
@@ -0,0 +1,3 @@
+const client_id = 'f9ddb7ab8b394323a2cf45a1bf95c4d9'; // Your client id
+const client_secret = '0a80a1e2e1724f68b703184c2388000c'; // Your secret
+module.exports = { client_id, client_secret };
